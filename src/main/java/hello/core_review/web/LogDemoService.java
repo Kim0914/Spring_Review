359deb207b9fc6f65f9d81fce666212814007583
kosa0914 <kosa0914@naver.com>
2022-01-23 23:52:55 +0900

    request scope 추가, Provider 추가

---
+++ src/main/java/hello/core_review/web/LogDemoService.java
@@ -0,0 +1,17 @@
+package hello.core_review.web;
+
+import hello.core_review.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String testID) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + testID);
+    }
+}
